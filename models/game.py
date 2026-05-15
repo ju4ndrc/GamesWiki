8d25c49e69a525891648451b7a60af30f27f5e2f
ju4ndrc <jdramirez90@ucatolica.edu.co>
2026-05-14 22:23:55 -0500

    [+]Triying to define the models, not complete

---
+++ models/game.py
@@ -0,0 +1,12 @@
+from typing import Optional
+from pydantic import BaseModel , Field
+
+
+class Game(BaseModel):
+    id:int
+    title:str
+    price:float
+    img:str
+    raiting:float
+    trailer:str | None
+    
