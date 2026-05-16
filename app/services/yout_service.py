7cc0af7ea0a36e65ef73b3b952e211e7d2023743
frutigeraerick <erei8377@gmail.com>
2026-05-16 17:39:45 -0500

    Replace httpx with requests for synchronous video fetch

--- app/services/yout_service.py
+++ app/services/yout_service.py
@@ -1,23 +1,20 @@
-
-import httpx
+import requests
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
 API_KEY = os.getenv("YOUTUBE_API_KEY")
 
-async def get_video(game_name):
+def get_video(game_name):
     
     BASE_URL = f"https://www.googleapis.com/youtube/v3/search?part=snippet&q={game_name}+trailer&key={API_KEY}"
     
-    
-    async with httpx.AsyncClient() as client:
-        response = await client.get(BASE_URL)
-        data = response.json()
+    response = requests.get(BASE_URL)
+    data = response.json()
         
     try:
         video_id = data["items"][0]["id"]["videoId"]
         return f"https://www.youtube.com/watch?v={video_id}"
     except:
         return "Problems finding the video"
-    
