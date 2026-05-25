32e5ec274806ccf6ab00a58c3c7c1da5e0f54c09
ju4ndrc <jdramirez90@ucatolica.edu.co>
2026-05-25 10:00:57 -0500

    [+]Fixing game router for async

--- app/router/game_router.py
+++ app/router/game_router.py
@@ -1,19 +1,20 @@
 from fastapi import APIRouter
 from app.services.cheap_service import get_game
 from app.services.raw_service import  get_info_game
-from app.services.yout_service import get_video
+# from app.services.yout_service import get_video
+from app.services.video import get_video
 from models.game import Game
 
 router = APIRouter(tags=["game_req"])
 
 @router.get('/search')
 async def search_game(searchGame:str):
-    info_game =  get_info_game(searchGame)
+    info_game = await  get_info_game(searchGame)
     
     results = []
     
     for game in info_game:
-        price =  get_game(game["name"])
+        price = await get_game(game["name"])
         
         trailer =  get_video(game["name"])
         
@@ -22,8 +23,10 @@ async def search_game(searchGame:str):
             "title":game["name"],
             "price":price,
             "img":game["image"],
-            "raiting":game["rating"],
+            "rating":game["rating"],
             "trailer":trailer,
+            "platforms":game["platforms"],
+            "genres":game["genres"]
     
         })
     return results
